bef7899d78460972ba9839d19d4a922048164f1b
nenicluka <nenicluka@elfak.rs>
2023-12-03 13:17:07 +0100

    update

--- src/controller/baseFunctions.ts
+++ src/controller/baseFunctions.ts
@@ -7,17 +7,29 @@ export const gridTableFunctions = (): void => {
     const icons = [...document.querySelectorAll(".icon")] as HTMLImageElement[]
     const startBtn = document.querySelector(".start-btn") as HTMLDivElement
 
+    const isMousePlaced$ = new BehaviorSubject<boolean>(false)
+    const isCheesePlaced$ = new BehaviorSubject<boolean>(false)
+
     // razmisli da l mozda da stavim da mi tableCells bude BehaviourSubject
     const isPlacingAllowed: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true)
     const tableCells = [...document.querySelectorAll(".cell")] as HTMLTableCellElement[]
     const grid: GridCell[][] = Array(15).fill(0).map(() => Array(40).fill(null))
 
-    // po deafaultu ne mogu elementi da se postavljaju preko drugih pa zato moras da disable
-    fromEvent(tableCells, "dragover")
-        .pipe(
-            tap((event) => event.preventDefault())
-        )
-        .subscribe()
+    isCheesePlaced$.subscribe({
+        next: (isPlaced: boolean) => {
+            const cheese = icons.filter(img => img.alt === "Cheese")[0]
+            cheese.style.opacity = isPlaced ? "0.5" : "1"
+            cheese.style.pointerEvents = isPlaced ? "none" : "auto"
+        }
+    })
+
+    isMousePlaced$.subscribe({
+        next: (isPlaced: boolean) => {
+            const mouse = icons.filter(img => img.alt === "Mouse")[0]
+            mouse.style.opacity = isPlaced ? "0.5" : "1"
+            mouse.style.pointerEvents = isPlaced ? "none" : "auto"
+        }
+    })
 
     // onemogucava da imas drag event za elemente koji se vec nalaze na tabeli
     fromEvent(tableCells, "dragstart")
@@ -35,18 +47,16 @@ export const gridTableFunctions = (): void => {
             const cell = event.currentTarget as HTMLTableCellElement
             if (!cell.childNodes.length) return
 
+            if ((cell.childNodes[0] as HTMLImageElement).alt === "Cheese")
+                isCheesePlaced$.next(false)
+            else if ((cell.childNodes[0] as HTMLImageElement).alt === "Mouse")
+                isMousePlaced$.next(false)
+
+            // brisanje
             grid[Number(cell.getAttribute("x"))][Number(cell.getAttribute("y"))] = null
             cell.innerHTML = ""
         })
     ).subscribe()
-
-    const selectedCell$: Observable<HTMLTableCellElement> = fromEvent(tableCells, "drop")
-        .pipe(
-            tap(() => {
-                isPlacingAllowed.next(true)
-            }),
-            map(event => event.currentTarget as HTMLTableCellElement)
-        )
 
     const selectedIcon$: Observable<HTMLImageElement> = fromEvent(icons, "dragstart")
         .pipe(
@@ -56,8 +66,20 @@ export const gridTableFunctions = (): void => {
             map(event => event.currentTarget as HTMLImageElement)
         )
 
-    // morace garant i da se uradi neki unsubsribe pogotov ako budem imao dragover 
-    // dobro ovde se mora cekira i situacija kad smo sigurni da je draggin over table, a ne lastValue
+    // po deafaultu ne mogu elementi da se postavljaju preko drugih pa zato moras da disable
+    fromEvent(tableCells, "dragover")
+        .pipe(
+            tap((event) => event.preventDefault())
+        )
+        .subscribe()
+
+    const selectedCell$: Observable<HTMLTableCellElement> = fromEvent(tableCells, "drop")
+        .pipe(
+            tap(() => {
+                isPlacingAllowed.next(true)
+            }),
+            map(event => event.currentTarget as HTMLTableCellElement)
+        )
 
     combineLatest([selectedCell$, selectedIcon$])
         .subscribe({
@@ -66,19 +88,27 @@ export const gridTableFunctions = (): void => {
                 const x: number = Number(cell.getAttribute("x"))
                 const y: number = Number(cell.getAttribute("y"))
 
+                if (selectedIcon.alt === "Cheese" && isCheesePlaced$.value ||
+                    selectedIcon.alt === "Mouse" && isMousePlaced$.value
+                )
+                    return
+
                 // mozda i ovo moze drugacije nekako
                 if (selectedIcon.alt === "Cheese") {
                     grid[x][y] = ItemType.Cheese
+                    isCheesePlaced$.next(true)
                 }
                 else if (selectedIcon.alt === "Wall") {
                     grid[x][y] = ItemType.Wall
                 }
                 else {
                     grid[x][y] = ItemType.Mouse
+                    isMousePlaced$.next(true)
                 }
 
                 const imgIcon = document.createElement("img")
                 imgIcon.classList.add("icon")
+                imgIcon.alt = selectedIcon.alt
                 imgIcon.src = selectedIcon.src
                 cell.appendChild(imgIcon)
                 isPlacingAllowed.next(false)
